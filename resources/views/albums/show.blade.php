e11d43c8d2280f9bc61b6f34dc9329d3c7c6a675
Kiki1985 <miroslav.dojchinovic@gmail.com>
2019-11-10 18:11:06 +0100

    addEventListener click in script tags

--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -5,20 +5,29 @@
 <div><a href="/albums"><button>Back</button></a><a href="/logout"><button>Logout</button></a></div>
 <hr>
 @for ($i = 1; $i <= $album->number_of_stickers; $i++)
-<div style="border: 1px solid LightBlue; margin: 2px; padding: 0 20px;">
-<p>{{$i}}
-<button onclick="insert({{$i}}, this)">+</button>
-<button onclick="insert({{$i}}, this)">-</button>
+<div style="border: 1px solid LightBlue; margin: 20px ; padding: 20px 20px; width: 120px">
+<div>{{$i}}
+<button id="{{$i.'plus'}}">+</button>
+<button id="{{$i.'minus'}}">-</button>
 <span class="{{$i}}">
 @if($stickers = DB::table('stickers')->where('album_id', $album->id)->where('sticker_id', $i)->exists())
 {{$stickers = DB::table('stickers')->where('album_id', $album->id)->where('sticker_id', $i)->value('sticker_number')}}
 @else
 {{$stickers = 0}}
 @endif
-</span></p></div>
+</span></div></div>
+<script type="text/javascript">
+document.getElementById("{{$i.'plus'}}").addEventListener("click", function() {
+  insert({{$i}}, this);
+  });
+document.getElementById("{{$i.'minus'}}").addEventListener("click", function() {
+  insert({{$i}}, this);
+});
+</script>
 @endfor
 <script type="text/javascript">
 var xhr = new XMLHttpRequest();
+
 function insert(sticker_id, obj){
 	var btn = obj.innerHTML;
 	var sticker_number = document.getElementsByClassName(sticker_id)[0].innerHTML;
@@ -38,6 +47,5 @@ function insert(sticker_id, obj){
 		}
 	}
 }
-
 </script>
 @endsection
