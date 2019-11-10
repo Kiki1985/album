d1b37848ae12f83fd4167f0636550f9b262c757b
Kiki1985 <miroslav.dojchinovic@gmail.com>
2019-11-10 12:19:48 +0100

    function store in StickersController updated

--- app/Http/Controllers/StickersController.php
+++ app/Http/Controllers/StickersController.php
@@ -1,25 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Album;
-use App\Sticker;
 use DB;
-use Illuminate\Http\Request;
-
 
 class StickersController extends Controller
 {
-    public function store(Sticker $sticker){
-        $album_id = request('album_id');
-        $sticker_number = request('sticker_number');
-        $sticker_id = request('sticker_id');
-
+    public function store(){
         DB::table('stickers')
             ->updateOrInsert(
-                ['album_id' => $album_id, 'sticker_id' => $sticker_id],
-                ['sticker_number' => $sticker_number]
+                ['album_id' => request('album_id'), 'sticker_id' => request('sticker_id')],
+                ['sticker_number' => request('sticker_number')]
             );
-
         return response();
     }
 }
