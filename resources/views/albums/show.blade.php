0e40d8f82ed6770f689932728954635ce39334a2
Kiki1985 <miroslav.dojchinovic@gmail.com>
2019-11-10 16:11:47 +0100

    XMLHttpRequest changed in albums/show.blade

--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -7,8 +7,8 @@
 @for ($i = 1; $i <= $album->number_of_stickers; $i++)
 <div style="border: 1px solid LightBlue; margin: 2px; padding: 0 20px;">
 <p>{{$i}}
-<button onclick="plusFunction({{$i}});window.location.reload();">+</button>
-<button onclick="minusFunction({{$i}});window.location.reload();">-</button>
+<button onclick="insert({{$i}}, this)">+</button>
+<button onclick="insert({{$i}}, this)">-</button>
 <span class="{{$i}}">
 @if($stickers = DB::table('stickers')->where('album_id', $album->id)->where('sticker_id', $i)->exists())
 {{$stickers = DB::table('stickers')->where('album_id', $album->id)->where('sticker_id', $i)->value('sticker_number')}}
@@ -18,32 +18,26 @@
 </span></p></div>
 @endfor
 <script type="text/javascript">
-function plusFunction(broj){
-	var num = document.getElementsByClassName(broj)[0].innerHTML;
-	num++;
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET','/albums/{album}/stickers?album_id={{$album->id}}&sticker_id='+broj+'&sticker_number='+num+'', true);
+var xhr = new XMLHttpRequest();
+function insert(sticker_id, obj){
+	var btn = obj.innerHTML;
+	var sticker_number = document.getElementsByClassName(sticker_id)[0].innerHTML;
+	if(btn === '+'){
+		sticker_number++;
+	}else{
+		sticker_number--;
+    	if(sticker_number<0){
+    		sticker_number=0;  
+    	}
+	}
+	xhr.open('GET','/albums/{album}/stickers?album_id={{$album->id}}&sticker_id='+sticker_id+'&sticker_number='+sticker_number+'', true);
 	xhr.send(); 
 	xhr.onreadystatechange = function(){
 		if(xhr.readyState === 4){
-		    //alert(xhr.responseText);
+		    window.location.reload();
 		}
 	}
 }
-function minusFunction(broj){
-    var num = document.getElementsByClassName(broj)[0].innerHTML;
-	num--;
-    if(num<0){
-    num=0;  
-    }
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET','/albums/{album}/stickers?album_id={{$album->id}}&sticker_id='+broj+'&sticker_number='+num+'', true);
-	xhr.send(); 
-	xhr.onreadystatechange = function(){
-		if(xhr.readyState === 4){
-		    //alert(xhr.responseText);
-		}
-	}
-}
+
 </script>
 @endsection
