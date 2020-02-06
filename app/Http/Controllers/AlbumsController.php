1331b727551f45f08a9e1ecbaa8c7326f151871c
Kiki1985 <miroslav.dojchinovic@gmail.com>
2020-02-06 15:00:16 +0100

    for loop moved from show.blade to AlbumsController

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -3,7 +3,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Album;
 use App\Sticker;
-use DB;
 class AlbumsController extends Controller
 {
     public function index(){
@@ -13,7 +12,15 @@ class AlbumsController extends Controller
 
     public function show($id){
         $album = Album::find($id);
-            return view('albums.show', compact('album','stickers'));
+        $stickerNumber = array();
+        for ($i = 1; $i <= $album->number_of_stickers; $i++){
+            if(Sticker::where('album_id', $album->id)->where('sticker_id', $i)->exists()){
+            array_push($stickerNumber, Sticker::where('album_id', $album->id)->where('sticker_id', $i)->value('sticker_number'));
+            }else{
+               array_push($stickerNumber, 0);
+            }
+        }   
+            return view('albums.show', compact('album','stickerNumber'));
     }
 
     public function store(){
--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -1,24 +1,16 @@
 @extends('layout')
-
 @section('content')
-
 <p><b><i>{{$album->name}}</i></b> Number of stickers: {{$album->number_of_stickers}}<span style="float: right">User {{Auth::user()->name}}</span></p>
-
 <div><a href="/albums"><button>Back</button></a>
 <a href="/logout"><button>Logout</button></a></div>
 <hr>
-
 @for ($i = 1; $i <= $album->number_of_stickers; $i++)
 <div style="border: 1px solid LightBlue; margin: 20px ; padding: 20px 20px; width: 90px">
 <div>{{$i}}
 <button id="{{$i.'minus'}}">-</button>
 <button id="{{$i.'plus'}}">+</button>
 <span id="{{$i}}">
-@if($stickers = DB::table('stickers')->where('album_id', $album->id)->where('sticker_id', $i)->exists())
-{{$stickers = DB::table('stickers')->where('album_id', $album->id)->where('sticker_id', $i)->value('sticker_number')}}
-@else
-{{$stickers = 0}}
-@endif
+{{$stickerNumber[$i-1]}}
 </span></div></div>
 <script type="text/javascript">
 document.getElementById("{{$i.'plus'}}").addEventListener("click", function() {
